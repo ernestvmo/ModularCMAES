6d32f9ec49cfd012957c5dc2881e496b1df8d161
ernestvmo <ernestvmo@gmail.com>
2023-06-01 11:04:21 +0200

    experiment 4 correction

--- benchmarks/benchmark_4.sh
+++ benchmarks/benchmark_4.sh
@@ -1,4 +1,4 @@
-echo 'BENCHMARK START'
+echo "BENCHMARK START"
 for i in {1..15}
 do
     echo iter $i
@@ -7,22 +7,20 @@ do
         echo problem $j
         for d in {5,20}
         do
-            python main.py -pid $j -iid $i -d $d -pt 2 -n 'SP-CMA[50]'
-            python main.py -pid $j -iid $i -d $d -pt 3 -n 'SP-CMA[20]'
-            python main.py -pid $j -iid $i -d $d -pt 4 -n 'SP-CMA[10]'
-
-            if [ $d -eq 5 ]; then
-                python main.py -pid $j -iid $i -d $d -pt 2 -n 'SP-CMA[50]-INFO(500)' -is 500 4
-                python main.py -pid $j -iid $i -d $d -pt 3 -n 'SP-CMA[20]-INFO(500)' -is 500 4
-                python main.py -pid $j -iid $i -d $d -pt 4 -n 'SP-CMA[10]-INFO(500)' -is 500 4
-            fi
-
-            if [ $d -eq 20 ]; then
-                python main.py -pid $j -iid $i -d $d -pt 2 -n 'SP-CMA[50]-INFO(500)' -is 1000 2
-                python main.py -pid $j -iid $i -d $d -pt 3 -n 'SP-CMA[20]-INFO(500)' -is 1000 2
-                python main.py -pid $j -iid $i -d $d -pt 4 -n 'SP-CMA[10]-INFO(500)' -is 1000 2
-            fi
+            python main.py -pid $j -iid $i -d $d -pt 2 -n "SP-CMA[50]"
+            python main.py -pid $j -iid $i -d $d -pt 3 -n "SP-CMA[20]"
+            python main.py -pid $j -iid $i -d $d -pt 4 -n "SP-CMA[10]"
+            
+            for t in {500,1000}
+            do
+                for r in {.1,.2}
+                do
+                    python main.py -pid $j -iid $i -d $d -pt 2 -n "SP-CMA[50]-INFO(${t}x${r})" -is $t $r
+                    python main.py -pid $j -iid $i -d $d -pt 3 -n "SP-CMA[20]-INFO(${t}x${r})" -is $t $r
+                    python main.py -pid $j -iid $i -d $d -pt 4 -n "SP-CMA[10]-INFO(${t}x${r})" -is $t $r
+                done
+            done
         done
     done
 done
-echo 'BENCHMARK COMPLETE'
+echo "BENCHMARK COMPLETE"

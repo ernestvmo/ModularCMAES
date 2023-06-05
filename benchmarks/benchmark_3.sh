1126ce8e6125f4d6b23ec20b5a2ac933353d139a
ernestvmo <ernestvmo@gmail.com>
2023-06-05 10:34:55 +0200

    combined exp 3 and exp 3.1 into one experimence (3)

--- benchmarks/benchmark_3.sh
+++ benchmarks/benchmark_3.sh
@@ -8,13 +8,21 @@ do
         do
             python main.py -pid $j -iid $i -d $d -pt 2 -n 'SP-CMA[50]'
             python main.py -pid $j -iid $i -d $d -pt 2 -n 'SP-CMA[50]-SVMi' -ic svm-i
+            python main.py -pid $j -iid $i -d $d -pt 2 -n 'SP-CMA[50]-LHS-SVMi' -im lhs -ic svm-i
             python main.py -pid $j -iid $i -d $d -pt 2 -n 'SP-CMA[50]-SVMo' -ic svm-o
+            python main.py -pid $j -iid $i -d $d -pt 2 -n 'SP-CMA[50]-LHS-SVMo' -im lhs -ic svm-o
+
             python main.py -pid $j -iid $i -d $d -pt 3 -n 'SP-CMA[20]'
             python main.py -pid $j -iid $i -d $d -pt 3 -n 'SP-CMA[20]-SVMi' -ic svm-i
-            python main.py -pid $j -iid $i -d $d -pt 3 -n 'SP-CMA[20]-SVMo' -ic svm-o
+            python main.py -pid $j -iid $i -d $d -pt 3 -n 'SP-CMA[20]-LHS-SVMi' -im lhs -ic svm-i
+            python main.py -pid $j -iid $i -d $d -pt 3 -n 'SP-CMA[20]-SVMo' -ic svm-o            
+            python main.py -pid $j -iid $i -d $d -pt 3 -n 'SP-CMA[20]-LHS-SVMo' -im lhs -ic svm-o
+            
             python main.py -pid $j -iid $i -d $d -pt 4 -n 'SP-CMA[10]'
             python main.py -pid $j -iid $i -d $d -pt 4 -n 'SP-CMA[10]-SVMi' -ic svm-i
+            python main.py -pid $j -iid $i -d $d -pt 4 -n 'SP-CMA[10]-LHS-SVMi' -im lhs -ic svm-i
             python main.py -pid $j -iid $i -d $d -pt 4 -n 'SP-CMA[10]-SVMo' -ic svm-o
+            python main.py -pid $j -iid $i -d $d -pt 4 -n 'SP-CMA[10]-LHS-SVMo' -im lhs -ic svm-o
         done
     done
 done
